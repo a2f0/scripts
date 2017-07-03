e744759d8b76dce3ad26bda46342c2e60bb26d7f
Dan Sullivan <dansullivan@gmail.com>
2017-07-03 18:07:50 +0000

    added certonly

---
+++ certbot-certonly.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+if [[ $# -lt 3 ]]; then
+  echo "Usage: $0 <webroot> <fqdn1> <fqdn2>"
+  echo "Example: $0 /opt/code/devopsrockstars-website/public/ devopsrockstars.com rose.devopsrockstars.com"
+  exit 1
+elif [ ! -e /usr/local/bin/certbot-auto ]; then
+  echo "certbot-auto not installed, exiting"
+  exit 1
+fi
+
+/usr/local/bin/certbot-auto certonly -n --webroot -w $1 -d $2 -d $3 --logs-dir=/opt/certbot/logs --config-dir=/opt/certbot/config --work-dir=/opt/certbot/work --email=webmaster@devopsrockstars.com --agree-tos --expand --hsts --post-hook 'apachectl restart' --debug
