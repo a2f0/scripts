6608ff62be0786fec977bee09c95aad2521c6de2
Dan Sullivan <dansullivan@gmail.com>
2017-07-03 17:31:25 +0000

    tarball created

--- backup-to-s3.sh
+++ backup-to-s3.sh
@@ -1,24 +1,26 @@
-#!/bin/sh
+#!/bin/bash
 
 if [[ $# -lt 3 ]]; then
   echo "Usage: $0 <path_to_folder> <s3_bucket_name> <zip_prefix>"
+  echo "Example: $0 /opt/code/devopsrockstars-website/public/paperclip devopsrockstars-web-backup devopsrockstars-paperclip-production"
+  exit 1
+elif [ ! -e ~/.aws/credentials ]; then
+  echo "~/.aws/credentials not set, exiting"
+  exit 1
+elif [ ! hash aws 2>/dev/null; then
+  echo "aws cli tools not installed, exiting"
   exit 1
 fi
 
-if [ ! -e ~/.aws/credentials ]; then 
-  echo "~/.aws/credentials not set, existing"
-  exit 1
-fi
+CWD=`pwd`
+FILE_NAME=$3-`date +%m`.`date +%d`.`date +%y`.`date +%s`.tar.gz
+MOST_RECENT=$3-mostrecent
 
 echo "Backing up: $1"
 echo "To S3 Bucket: $2"
 echo "Prefix: $3"
 
 cd /tmp
-tar -czvf $3-`date +%m`.`date +%d`.`date +%y`.tar.gz $1
+tar -czf $FILE_NAME.tar.gz $1
 
-#tar -czvf /mnt/devopsrockstars-web-backup/paperclip-production-`date +%m`.`date +%d`.`date +%y`.tar.gz paperclip && rm -rf /mnt/devopsrockstars-web-backup/paperclip-production-latest.tar.gz && ln -s /mnt/devopsrockstars-web-backup/paperclip-production-`date +%m`.`date +%d`.`date +%y`.tar.gz /mnt/devopsrockstars-web-backup/paperclip-production-latest.tar.gz
-
-#echo 'Hello World' > /tmp/file.txt
-#aws s3 cp /tmp/file.txt s3://dirname/dirsubfolder/file.txt
-#rm /tmp/file.txt
+cd $CWD; unset CWD
